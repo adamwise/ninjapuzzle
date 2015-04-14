cc853abf02fce13058a0269aef53f9492b3c13a6
adamvarga25 <adamwise@windowslive.com>
2015-04-14 17:32:28 +0200

    add some style

--- gruntfile.js
+++ gruntfile.js
@@ -14,7 +14,6 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['sass']);
 
