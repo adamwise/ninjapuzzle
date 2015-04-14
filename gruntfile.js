cf47a5f41e7f665ca8bc520077388250cb8d5665
adamvarga25 <adamwise@windowslive.com>
2015-04-14 17:27:30 +0200

    first test commit

---
+++ gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
+        },
+        files: {                         // Dictionary of files
+          'css/style.css': 'scss/main.scss',       // 'destination': 'source'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass']);
+
+};
